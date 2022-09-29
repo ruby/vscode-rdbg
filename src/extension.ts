d8cef0a98707a30c813819573018d6a524a303ae
firien <58729+firien@users.noreply.github.com>
2022-09-28 22:44:56 -0400

    typo fix

--- src/extension.ts
+++ src/extension.ts
@@ -124,7 +124,7 @@ class RdbgDebugAdapterTrackerFactory implements vscode.DebugAdapterTrackerFactor
 				}
 			},
 			onError(e) {
-				outputChannel.appendLine("[Error on seession]\n" + e.name + ": " + e.message + "\ne: " + JSON.stringify(e));
+				outputChannel.appendLine("[Error on session]\n" + e.name + ": " + e.message + "\ne: " + JSON.stringify(e));
 			}
 		};
 		if (session.configuration.showProtocolLog) {
