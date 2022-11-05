70397f9edbc00a754dce663ee8ef5719c37aed24
Naoto Ono <onoto1998@gmail.com>
2022-11-05 13:49:45 +0900

    fix

--- src/extension.ts
+++ src/extension.ts
@@ -557,7 +557,7 @@ class RdbgAdapterDescriptorFactory implements DebugAdapterDescriptorFactory {
 
 				await new Promise((resolve) => {
 
-					const p = child_process.exec("rdbg " + "--command -- ruby" + exec_args);
+					const p = child_process.exec("rdbg " + "--command -- ruby " + exec_args);
 					let path: string;
 
 					p.on('error', e => {
