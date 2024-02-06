f7837f32ee82574234c62fb85408d5722e45422b
Naoto Ono <onoto1998@gmail.com>
2024-02-06 12:45:18 +0900

    Add launchable into CI

--- src/ui-test/config.ts
+++ src/ui-test/config.ts
@@ -1,7 +1,15 @@
 import { MochaOptions } from "vscode-extension-tester";
 
 const options: MochaOptions = {
-	timeout: 180000,
+    timeout: 180000,
 };
+
+const testReports = process.env.LAUNCHABLE_TEST_REPORTS;
+if (testReports) {
+    options.reporter = "mocha-junit-reporter";
+    options.reporterOptions = {
+        mochaFile: testReports,
+    };
+}
 
 module.exports = options;
