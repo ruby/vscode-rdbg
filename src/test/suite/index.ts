87dc072d4c1a0881f9535389abc97adda3111007
Naoto Ono <onoto1998@gmail.com>
2024-03-11 10:50:51 +0900

    Bump glob from 8.1.0 to 10.3.10

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,49 +1,44 @@
 import * as path from "path";
 import * as Mocha from "mocha";
-import * as glob from "glob";
+import { glob } from "glob";
 import { MochaOptions } from "mocha";
 
 export function run(): Promise<void> {
-	const mochaOpts: MochaOptions = {
-		ui: "tdd",
-		color: true
-	}
-	const testReports = process.env.LAUNCHABLE_TEST_REPORTS
-	if (testReports) {
-		mochaOpts.reporter = 'mocha-junit-reporter';
-		mochaOpts.reporterOptions = {
-			mochaFile: testReports
-		}
-	}
-	// Create the mocha test
-	const mocha = new Mocha(mochaOpts);
+    const mochaOpts: MochaOptions = {
+        ui: "tdd",
+        color: true,
+    };
+    const testReports = process.env.LAUNCHABLE_TEST_REPORTS;
+    if (testReports) {
+        mochaOpts.reporter = "mocha-junit-reporter";
+        mochaOpts.reporterOptions = {
+            mochaFile: testReports,
+        };
+    }
+    // Create the mocha test
+    const mocha = new Mocha(mochaOpts);
 
-	mocha.timeout("20000");
+    mocha.timeout("20000");
 
-	const testsRoot = path.resolve(__dirname, "..");
+    const testsRoot = path.resolve(__dirname, "..");
 
-	return new Promise((c, e) => {
-		glob("**/**.test.js", { cwd: testsRoot }, (err, files) => {
-			if (err) {
-				return e(err);
-			}
+    return new Promise(async (c, e) => {
+        const files = await glob("**/**.test.js", { cwd: testsRoot });
+        // Add files to the test suite
+        files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
 
-			// Add files to the test suite
-			files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
-
-			try {
-				// Run the mocha test
-				mocha.run(failures => {
-					if (failures > 0) {
-						e(new Error(`${failures} tests failed.`));
-					} else {
-						c();
-					}
-				});
-			} catch (err) {
-				console.error(err);
-				e(err);
-			}
-		});
-	});
+        try {
+            // Run the mocha test
+            mocha.run((failures) => {
+                if (failures > 0) {
+                    e(new Error(`${failures} tests failed.`));
+                } else {
+                    c();
+                }
+            });
+        } catch (err) {
+            console.error(err);
+            e(err);
+        }
+    });
 }
